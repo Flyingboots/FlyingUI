883b3bf7211040b6eb3214a7549c85da751d8522
Tobias Rundquist <superanka@hotmail.com>
2011-05-06 16:41:47 +0200

    Powerbar fix.

---
+++ FlyingUI/Tukui/modules/datatext/currency.lua
@@ -0,0 +1,45 @@
+local T, C, L = unpack(select(2, ...)) -- Import: T - functions, constants, variables; C - config; L - locales
+--------------------------------------------------------------------
+ -- CURRENCY
+--------------------------------------------------------------------
+
+if C["datatext"].currency and C["datatext"].currency > 0 then
+	local Stat = CreateFrame("Frame")
+	Stat:EnableMouse(true)
+	Stat:SetFrameStrata("BACKGROUND")
+	Stat:SetFrameLevel(3)
+
+	local Text  = TukuiInfoLeft:CreateFontString(nil, "OVERLAY")
+	Text:SetFont(C.media.pixelfont, C["datatext"].fontsize, "THINOUTLINE")
+	T.PP(C["datatext"].currency, Text)
+	local c = T.oUF_colors.class[T.myclass]
+	Text:SetTextColor(c[1], c[2], c[3])
+	
+	local function update()
+		local _text = "---"
+		for i = 1, MAX_WATCHED_TOKENS do
+			local name, count, _, _, _ = GetBackpackCurrencyInfo(i)
+			if name and count then
+				if(i ~= 1) then _text = _text .. " " else _text = "" end
+				words = { strsplit(" ", name) }
+				for _, word in ipairs(words) do
+					_text = _text .. string.sub(word,1,1)
+				end
+				_text = _text .. ": " .. count
+			end
+		end
+		
+		Text:SetText(_text)
+	end
+	
+	local function OnEvent(self, event, ...)
+		update()
+		self:SetAllPoints(Text)
+		Stat:UnregisterEvent("PLAYER_LOGIN")	
+	end
+
+	Stat:RegisterEvent("PLAYER_LOGIN")	
+	hooksecurefunc("BackpackTokenFrame_Update", update)
+	Stat:SetScript("OnEvent", OnEvent)
+	Stat:SetScript("OnMouseDown", function() ToggleCharacter("TokenFrame") end)
+end
